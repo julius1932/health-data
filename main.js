97c46250e864526383464092892270ad24619eaf
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-07-06 10:02:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+var med_app = require("./medication/app");
+var insu_app = require("./insurance/app");
+var prov_app = require("./providers/app");
+
+app.set('port', process.env.PORT || 3000);
+
+app.use('/medication', med_app);
+app.use('/insurance', insu_app);
+app.use('/providers', prov_app);
+
+app.get('/', function (req, res) {
+  res.send("This is the '/' route in main_app");
+});
+
+
+app.listen(app.get('port'));
+console.log("server listening on port " + app.get('port'));
